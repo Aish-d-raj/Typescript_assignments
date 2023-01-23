f0f4d466ad95558ad5f4531147fbd8ee0a60dfc8
Aish-d-raj <115470434+Aish-d-raj@users.noreply.github.com>
2023-01-23 17:01:22 +0530

    Add files via upload

---
+++ Mobilecart/array.js
@@ -0,0 +1,8 @@
+var manufacturers = [{ pId: 432,
+        productName: "Samsung Galaxy Note 7",
+        productPrice: 59000,
+        productAvailable: true,
+        imageUrl: "SamsungGalaxy_Gold.jpg",
+        productDescription: "Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory."
+    }
+];
